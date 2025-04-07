f5f2101c914c76bd14291e97985ab6f5f0f08e66
haruki7049 <tontonkirikiri@gmail.com>
2025-04-07 17:21:58 +0900

    Add lispImpls

--- example/default.nix
+++ example/default.nix
@@ -11,25 +11,25 @@
 }:
 
 let
-  alexandria =  (fetchFromGitLab {
+  alexandria = fetchFromGitLab {
     domain = "gitlab.common-lisp.net";
     repo = "alexandria";
     owner = "alexandria";
     rev = "8514d8e68ed0c733abf7f96f9e91b24912686dc4";
     hash = "sha256-vPHp/dXX24zUPF1t7EdBryzqlG33A0fOoD5loFOxAEs=";
-  });
+  };
 
-  asdf-flv = (fetchFromGitHub {
+  asdf-flv = fetchFromGitHub {
     repo = "asdf-flv";
     owner = "didierverna";
     rev = "3f1de416f7f40a39c47f08335c710a884ece36b3";
     hash = "sha256-XKMv04SNF7brzLReTSe9GJBaLJITDYUPyF41TRuUbOs=";
-  });
+  };
 
-  trivial-backtrace = (fetchzip {
+  trivial-backtrace = fetchzip {
     url = "http://common-lisp.net/project/trivial-backtrace/trivial-backtrace.tar.gz";
     hash = "sha256-0enXFClfULa7iPagwD9x5ueaJPBcLURp1vuhGMyPZUs=";
-  });
+  };
 in
 
 stdenv.mkDerivation rec {
@@ -46,7 +46,9 @@ stdenv.mkDerivation rec {
 
     export HOME=$TMPDIR
 
-    cat "${builderScript}" | ${sbcl}/bin/sbcl
+    ${lib.strings.concatStringsSep "\n" (
+      builtins.map (drv: "cat '${builderScript}' | ${lib.getExe drv}") lispImpls
+    )}
 
     runHook postBuild
   '';
@@ -62,8 +64,8 @@ stdenv.mkDerivation rec {
 
   CL_SOURCE_REGISTRY = "${alexandria}:${asdf-flv}:${trivial-backtrace}:${src}";
 
-  #lispImpls = [
-  #  sbcl
-  #  clisp
-  #];
+  lispImpls = [
+    sbcl
+    clisp
+  ];
 }
