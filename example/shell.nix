719e61791c1a795ebdd08796cde6408b6cde8c5a
haruki7049 <tontonkirikiri@gmail.com>
2025-04-07 22:56:11 +0900

    Update

--- example/shell.nix
+++ example/shell.nix
@@ -1,39 +1,47 @@
 {
   lib,
-  stdenv,
   mkShell,
-  fetchurl,
+  fetchzip,
+  fetchFromGitHub,
   fetchFromGitLab,
 
   sbcl,
 }:
 
 let
-  source-registry = stdenv.mkDerivation rec {
-    name = "source-registry";
-    dontUnpack = true;
+  makeCommonLispSourceRegistry = drv-list: lib.strings.concatStringsSep ":" (builtins.map (drv: "${drv}") drv-list);
 
-    srcs = [
-      (fetchFromGitLab {
-        domain = "gitlab.common-lisp.net";
-        repo = "alexandria";
-        owner = "alexandria";
-        rev = "8514d8e68ed0c733abf7f96f9e91b24912686dc4";
-        hash = "sha256-vPHp/dXX24zUPF1t7EdBryzqlG33A0fOoD5loFOxAEs=";
-      })
-    ];
+  alexandria = fetchFromGitLab {
+    domain = "gitlab.common-lisp.net";
+    repo = "alexandria";
+    owner = "alexandria";
+    rev = "8514d8e68ed0c733abf7f96f9e91b24912686dc4";
+    hash = "sha256-vPHp/dXX24zUPF1t7EdBryzqlG33A0fOoD5loFOxAEs=";
+  };
 
-    installPhase = ''
-      mkdir $out
-      cp -r ${lib.strings.concatStringsSep " " (builtins.map (drv: "${drv}/*") srcs)} $out
-    '';
+  asdf-flv = fetchFromGitHub {
+    repo = "asdf-flv";
+    owner = "didierverna";
+    rev = "3f1de416f7f40a39c47f08335c710a884ece36b3";
+    hash = "sha256-XKMv04SNF7brzLReTSe9GJBaLJITDYUPyF41TRuUbOs=";
+  };
+
+  trivial-backtrace = fetchzip {
+    url = "http://common-lisp.net/project/trivial-backtrace/trivial-backtrace.tar.gz";
+    hash = "sha256-0enXFClfULa7iPagwD9x5ueaJPBcLURp1vuhGMyPZUs=";
   };
 in
 
-mkShell {
+mkShell rec {
   packages = [
     sbcl
   ];
 
-  CL_SOURCE_REGISTRY = "${source-registry}";
+  buildInputs = [
+    alexandria
+    asdf-flv
+    trivial-backtrace
+  ];
+
+  CL_SOURCE_REGISTRY = makeCommonLispSourceRegistry buildInputs;
 }
