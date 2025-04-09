0b27586e188e0c50ec35f4e4ba9c199851fe72f0
haruki7049 <tontonkirikiri@gmail.com>
2025-04-10 01:13:58 +0900

    Update

--- example/default.nix
+++ example/default.nix
@@ -1,3 +1,5 @@
+# Run `nix-build -E 'with import <nixpkgs> { overlays = [ (import ../.) ]; }; callPackage ./. { }'`
+
 {
   lib,
   callPackage,
@@ -12,24 +14,63 @@
 }:
 
 let
-  alexandria = fetchFromGitLab {
-    domain = "gitlab.common-lisp.net";
-    repo = "alexandria";
-    owner = "alexandria";
-    rev = "8514d8e68ed0c733abf7f96f9e91b24912686dc4";
-    hash = "sha256-vPHp/dXX24zUPF1t7EdBryzqlG33A0fOoD5loFOxAEs=";
+  alexandria = lispDerivation {
+    name = "alexandria";
+    src = fetchFromGitLab {
+      domain = "gitlab.common-lisp.net";
+      repo = "alexandria";
+      owner = "alexandria";
+      rev = "8514d8e68ed0c733abf7f96f9e91b24912686dc4";
+      hash = "sha256-vPHp/dXX24zUPF1t7EdBryzqlG33A0fOoD5loFOxAEs=";
+    };
+
+    lisps = [
+      sbcl
+    ];
+
+    doCheck = true;
+
+    runner = sbcl;
   };
 
-  asdf-flv = fetchFromGitHub {
-    repo = "asdf-flv";
-    owner = "didierverna";
-    rev = "3f1de416f7f40a39c47f08335c710a884ece36b3";
-    hash = "sha256-XKMv04SNF7brzLReTSe9GJBaLJITDYUPyF41TRuUbOs=";
+  asdf-flv = lispDerivation {
+    name = "net.didierverna.asdf-flv";
+    src = fetchFromGitHub {
+      owner = "didierverna";
+      repo = "asdf-flv";
+      rev = "3f1de416f7f40a39c47f08335c710a884ece36b3";
+      hash = "sha256-XKMv04SNF7brzLReTSe9GJBaLJITDYUPyF41TRuUbOs=";
+    };
+
+    lisps = [
+      sbcl
+      ecl
+      clisp
+    ];
+
+    doCheck = true;
+
+    runner = sbcl;
   };
 
-  trivial-backtrace = fetchzip {
-    url = "http://common-lisp.net/project/trivial-backtrace/trivial-backtrace.tar.gz";
-    hash = "sha256-0enXFClfULa7iPagwD9x5ueaJPBcLURp1vuhGMyPZUs=";
+  trivial-backtrace = lispDerivation {
+    name = "trivial-backtrace";
+    src = fetchFromGitHub {
+      owner = "hraban";
+      repo = "trivial-backtrace";
+      rev = "7f90b4a4144775cca0728791e4b92ac2557b07a1";
+      hash = "sha256-dg5xWOdR1NuRjgJq6KBwbrNxS2ZKH94JwsXa+va4QIY=";
+    };
+
+    lisps = [
+      sbcl
+      clisp
+      ecl
+    ];
+
+    runner = sbcl;
+
+    dontCheck = true;
   };
 in
 
@@ -38,15 +79,17 @@ lispDerivation {
   version = "dev";
   src = lib.cleanSource ./.;
 
-  buildInputs = [
+  lispLibs = [
     alexandria
     asdf-flv
     trivial-backtrace
   ];
 
-  lispImpls = [
+  lisps = [
     sbcl
     clisp
     ecl
   ];
+
+  runner = sbcl;
 }
